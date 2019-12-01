e7ded72dd275efc9ae40805a2945a16a495a55fa
PHBPLRS <846223176@163.com>
2019-11-30 19:07:18 -0800

    myth

---
+++ myth.sh
@@ -0,0 +1,301 @@
+solc use 0.4.13
+echo [FILE] WyvernAtomicizer.sol | tee -a myth.log 
+myth analyze WyvernAtomicizer.sol >> myth.log 2>&1
+solc use 0.4.17
+echo [FILE] casinoProxy.sol | tee -a myth.log 
+myth analyze casinoProxy.sol >> myth.log 2>&1
+solc use 0.4.18
+echo [FILE] KyberNetworkProxy.sol | tee -a myth.log 
+myth analyze KyberNetworkProxy.sol >> myth.log 2>&1
+solc use 0.4.21
+echo [FILE] UserRank.sol | tee -a myth.log 
+myth analyze UserRank.sol >> myth.log 2>&1
+echo [FILE] ICOStartReservation.sol | tee -a myth.log 
+myth analyze ICOStartReservation.sol >> myth.log 2>&1
+echo [FILE] MineableToken.sol | tee -a myth.log 
+myth analyze MineableToken.sol >> myth.log 2>&1
+echo [FILE] Updater.sol | tee -a myth.log 
+myth analyze Updater.sol >> myth.log 2>&1
+echo [FILE] CandyLand.sol | tee -a myth.log 
+myth analyze CandyLand.sol >> myth.log 2>&1
+echo [FILE] CandyLandSale.sol | tee -a myth.log 
+myth analyze CandyLandSale.sol >> myth.log 2>&1
+echo [FILE] AuctusTokenSale.sol | tee -a myth.log 
+myth analyze AuctusTokenSale.sol >> myth.log 2>&1
+echo [FILE] RUNEToken.sol | tee -a myth.log 
+myth analyze RUNEToken.sol >> myth.log 2>&1
+solc use 0.4.23
+echo [FILE] GetDecimals.sol | tee -a myth.log 
+myth analyze GetDecimals.sol >> myth.log 2>&1
+echo [FILE] RegistryExec.sol | tee -a myth.log 
+myth analyze RegistryExec.sol >> myth.log 2>&1
+echo [FILE] MonarchyController.sol | tee -a myth.log 
+myth analyze MonarchyController.sol >> myth.log 2>&1
+echo [FILE] ParsecInitialShip.sol | tee -a myth.log 
+myth analyze ParsecInitialShip.sol >> myth.log 2>&1
+solc use 0.4.24
+echo [FILE] IssuanceController.sol | tee -a myth.log 
+myth analyze IssuanceController.sol >> myth.log 2>&1
+echo [FILE] AWinner.sol | tee -a myth.log 
+myth analyze AWinner.sol >> myth.log 2>&1
+echo [FILE] AddressDeployer.sol | tee -a myth.log 
+myth analyze AddressDeployer.sol >> myth.log 2>&1
+echo [FILE] AddressToken.sol | tee -a myth.log 
+myth analyze AddressToken.sol >> myth.log 2>&1
+echo [FILE] AdminUpgradeabilityProxy.sol | tee -a myth.log 
+myth analyze AdminUpgradeabilityProxy.sol >> myth.log 2>&1
+echo [FILE] AstraBasicMultiTokenDeployer.sol | tee -a myth.log 
+myth analyze AstraBasicMultiTokenDeployer.sol >> myth.log 2>&1
+echo [FILE] AstraMultiTokenDeployer.sol | tee -a myth.log 
+myth analyze AstraMultiTokenDeployer.sol >> myth.log 2>&1
+echo [FILE] BATMO.sol | tee -a myth.log 
+myth analyze BATMO.sol >> myth.log 2>&1
+echo [FILE] BBFarm.sol | tee -a myth.log 
+myth analyze BBFarm.sol >> myth.log 2>&1
+echo [FILE] BBLibV7.sol | tee -a myth.log 
+myth analyze BBLibV7.sol >> myth.log 2>&1
+echo [FILE] BancorBuyer.sol | tee -a myth.log 
+myth analyze BancorBuyer.sol >> myth.log 2>&1
+echo [FILE] BasicMultiTokenDeployer.sol | tee -a myth.log 
+myth analyze BasicMultiTokenDeployer.sol >> myth.log 2>&1
+echo [FILE] BigOne.sol | tee -a myth.log 
+myth analyze BigOne.sol >> myth.log 2>&1
+echo [FILE] CAE4Dlong.sol | tee -a myth.log 
+myth analyze CAE4Dlong.sol >> myth.log 2>&1
+echo [FILE] CDPCreator.sol | tee -a myth.log 
+myth analyze CDPCreator.sol >> myth.log 2>&1
+echo [FILE] CappedSTOFactory.sol | tee -a myth.log 
+myth analyze CappedSTOFactory.sol >> myth.log 2>&1
+echo [FILE] ChivesGarden.sol | tee -a myth.log 
+myth analyze ChivesGarden.sol >> myth.log 2>&1
+echo [FILE] CountTransferManagerFactory.sol | tee -a myth.log 
+myth analyze CountTransferManagerFactory.sol >> myth.log 2>&1
+echo [FILE] CvcProxy.sol | tee -a myth.log 
+myth analyze CvcProxy.sol >> myth.log 2>&1
+echo [FILE] DogScam.sol | tee -a myth.log 
+myth analyze DogScam.sol >> myth.log 2>&1
+echo [FILE] Duang8.sol | tee -a myth.log 
+myth analyze Duang8.sol >> myth.log 2>&1
+echo [FILE] EnsOwnerProxy.sol | tee -a myth.log 
+myth analyze EnsOwnerProxy.sol >> myth.log 2>&1
+echo [FILE] ExitFraud.sol | tee -a myth.log 
+myth analyze ExitFraud.sol >> myth.log 2>&1
+echo [FILE] F3DClick.sol | tee -a myth.log 
+myth analyze F3DClick.sol >> myth.log 2>&1
+echo [FILE] F3DGame.sol | tee -a myth.log 
+myth analyze F3DGame.sol >> myth.log 2>&1
+echo [FILE] F3DGo.sol | tee -a myth.log 
+myth analyze F3DGo.sol >> myth.log 2>&1
+echo [FILE] F3DGoQuick.sol | tee -a myth.log 
+myth analyze F3DGoQuick.sol >> myth.log 2>&1
+echo [FILE] F3DKeysCalcShort.sol | tee -a myth.log 
+myth analyze F3DKeysCalcShort.sol >> myth.log 2>&1
+echo [FILE] F3DLink.sol | tee -a myth.log 
+myth analyze F3DLink.sol >> myth.log 2>&1
+echo [FILE] F3DNEW.sol | tee -a myth.log 
+myth analyze F3DNEW.sol >> myth.log 2>&1
+echo [FILE] F3DPLUS.sol | tee -a myth.log 
+myth analyze F3DPLUS.sol >> myth.log 2>&1
+echo [FILE] F3DSHORT.sol | tee -a myth.log 
+myth analyze F3DSHORT.sol >> myth.log 2>&1
+echo [FILE] F3DSOON.sol | tee -a myth.log 
+myth analyze F3DSOON.sol >> myth.log 2>&1
+echo [FILE] F3DShop.sol | tee -a myth.log 
+myth analyze F3DShop.sol >> myth.log 2>&1
+echo [FILE] F3DShopQuick.sol | tee -a myth.log 
+myth analyze F3DShopQuick.sol >> myth.log 2>&1
+echo [FILE] F3Dultra.sol | tee -a myth.log 
+myth analyze F3Dultra.sol >> myth.log 2>&1
+echo [FILE] F4Kings.sol | tee -a myth.log 
+myth analyze F4Kings.sol >> myth.log 2>&1
+echo [FILE] F5D.sol | tee -a myth.log 
+myth analyze F5D.sol >> myth.log 2>&1
+echo [FILE] FJ3DLong.sol | tee -a myth.log 
+myth analyze FJ3DLong.sol >> myth.log 2>&1
+echo [FILE] FJFoMo3DLong.sol | tee -a myth.log 
+myth analyze FJFoMo3DLong.sol >> myth.log 2>&1
+echo [FILE] FOMO3.sol | tee -a myth.log 
+myth analyze FOMO3.sol >> myth.log 2>&1
+echo [FILE] FOMO3TEST.sol | tee -a myth.log 
+myth analyze FOMO3TEST.sol >> myth.log 2>&1
+echo [FILE] FOMO3TEST1.sol | tee -a myth.log 
+myth analyze FOMO3TEST1.sol >> myth.log 2>&1
+echo [FILE] FOMOO.sol | tee -a myth.log 
+myth analyze FOMOO.sol >> myth.log 2>&1
+echo [FILE] FOMOQuick.sol | tee -a myth.log 
+myth analyze FOMOQuick.sol >> myth.log 2>&1
+echo [FILE] FengJinFoMo3D.sol | tee -a myth.log 
+myth analyze FengJinFoMo3D.sol >> myth.log 2>&1
+echo [FILE] FoMo100.sol | tee -a myth.log 
+myth analyze FoMo100.sol >> myth.log 2>&1
+echo [FILE] FoMo3DFast.sol | tee -a myth.log 
+myth analyze FoMo3DFast.sol >> myth.log 2>&1
+echo [FILE] FoMo3DLight.sol | tee -a myth.log 
+myth analyze FoMo3DLight.sol >> myth.log 2>&1
+echo [FILE] FoMo3DLightning.sol | tee -a myth.log 
+myth analyze FoMo3DLightning.sol >> myth.log 2>&1
+echo [FILE] FoMo3DPlus.sol | tee -a myth.log 
+myth analyze FoMo3DPlus.sol >> myth.log 2>&1
+echo [FILE] FoMo3DUnlimited.sol | tee -a myth.log 
+myth analyze FoMo3DUnlimited.sol >> myth.log 2>&1
+echo [FILE] FoMo3DlongUnlimited.sol | tee -a myth.log 
+myth analyze FoMo3DlongUnlimited.sol >> myth.log 2>&1
+echo [FILE] FoMo3Dshort.sol | tee -a myth.log 
+myth analyze FoMo3Dshort.sol >> myth.log 2>&1
+echo [FILE] FoMo3DshortAgain.sol | tee -a myth.log 
+myth analyze FoMo3DshortAgain.sol >> myth.log 2>&1
+echo [FILE] FoMo3Dsmall.sol | tee -a myth.log 
+myth analyze FoMo3Dsmall.sol >> myth.log 2>&1
+echo [FILE] FoMo3Dthunder.sol | tee -a myth.log 
+myth analyze FoMo3Dthunder.sol >> myth.log 2>&1
+echo [FILE] FoMoGame.sol | tee -a myth.log 
+myth analyze FoMoGame.sol >> myth.log 2>&1
+echo [FILE] FoMoRapid.sol | tee -a myth.log 
+myth analyze FoMoRapid.sol >> myth.log 2>&1
+echo [FILE] Fomo12H.sol | tee -a myth.log 
+myth analyze Fomo12H.sol >> myth.log 2>&1
+echo [FILE] Fomo3DQuick.sol | tee -a myth.log 
+myth analyze Fomo3DQuick.sol >> myth.log 2>&1
+echo [FILE] Fomo3H.sol | tee -a myth.log 
+myth analyze Fomo3H.sol >> myth.log 2>&1
+echo [FILE] Fomo60Min.sol | tee -a myth.log 
+myth analyze Fomo60Min.sol >> myth.log 2>&1
+echo [FILE] FomoDD.sol | tee -a myth.log 
+myth analyze FomoDD.sol >> myth.log 2>&1
+echo [FILE] FomoSuper.sol | tee -a myth.log 
+myth analyze FomoSuper.sol >> myth.log 2>&1
+echo [FILE] FomoXP.sol | tee -a myth.log 
+myth analyze FomoXP.sol >> myth.log 2>&1
+echo [FILE] FuMoToken.sol | tee -a myth.log 
+myth analyze FuMoToken.sol >> myth.log 2>&1
+echo [FILE] FullFOMO.sol | tee -a myth.log 
+myth analyze FullFOMO.sol >> myth.log 2>&1
+echo [FILE] GameOfSwords.sol | tee -a myth.log 
+myth analyze GameOfSwords.sol >> myth.log 2>&1
+echo [FILE] GoalbonanzaPlus.sol | tee -a myth.log 
+myth analyze GoalbonanzaPlus.sol >> myth.log 2>&1
+echo [FILE] GoalbonanzaPlusLong.sol | tee -a myth.log 
+myth analyze GoalbonanzaPlusLong.sol >> myth.log 2>&1
+echo [FILE] GrandTheftFOMO.sol | tee -a myth.log 
+myth analyze GrandTheftFOMO.sol >> myth.log 2>&1
+echo [FILE] H3FoMo3Dlong.sol | tee -a myth.log 
+myth analyze H3FoMo3Dlong.sol >> myth.log 2>&1
+echo [FILE] JanKenPon.sol | tee -a myth.log 
+myth analyze JanKenPon.sol >> myth.log 2>&1
+echo [FILE] LEGALBLOCK.sol | tee -a myth.log 
+myth analyze LEGALBLOCK.sol >> myth.log 2>&1
+echo [FILE] LOLlong.sol | tee -a myth.log 
+myth analyze LOLlong.sol >> myth.log 2>&1
+echo [FILE] LastUnicorn.sol | tee -a myth.log 
+myth analyze LastUnicorn.sol >> myth.log 2>&1
+echo [FILE] MC2long.sol | tee -a myth.log 
+myth analyze MC2long.sol >> myth.log 2>&1
+echo [FILE] MonkeyScam.sol | tee -a myth.log 
+myth analyze MonkeyScam.sol >> myth.log 2>&1
+echo [FILE] MultiBuyer.sol | tee -a myth.log 
+myth analyze MultiBuyer.sol >> myth.log 2>&1
+echo [FILE] MultiSeller.sol | tee -a myth.log 
+myth analyze MultiSeller.sol >> myth.log 2>&1
+echo [FILE] NewChance.sol | tee -a myth.log 
+myth analyze NewChance.sol >> myth.log 2>&1
+echo [FILE] OK3D.sol | tee -a myth.log 
+myth analyze OK3D.sol >> myth.log 2>&1
+echo [FILE] OwnedUpgradeabilityProxy.sol | tee -a myth.log 
+myth analyze OwnedUpgradeabilityProxy.sol >> myth.log 2>&1
+echo [FILE] POHMO.sol | tee -a myth.log 
+myth analyze POHMO.sol >> myth.log 2>&1
+echo [FILE] POOHMO.sol | tee -a myth.log 
+myth analyze POOHMO.sol >> myth.log 2>&1
+echo [FILE] POOHMOX.sol | tee -a myth.log 
+myth analyze POOHMOX.sol >> myth.log 2>&1
+echo [FILE] PercentageTransferManagerFactory.sol | tee -a myth.log 
+myth analyze PercentageTransferManagerFactory.sol >> myth.log 2>&1
+echo [FILE] PlayCoinKey.sol | tee -a myth.log 
+myth analyze PlayCoinKey.sol >> myth.log 2>&1
+echo [FILE] PoHMO4D.sol | tee -a myth.log 
+myth analyze PoHMO4D.sol >> myth.log 2>&1
+echo [FILE] PoaCrowdsale.sol | tee -a myth.log 
+myth analyze PoaCrowdsale.sol >> myth.log 2>&1
+echo [FILE] PoaManager.sol | tee -a myth.log 
+myth analyze PoaManager.sol >> myth.log 2>&1
+echo [FILE] PoaToken.sol | tee -a myth.log 
+myth analyze PoaToken.sol >> myth.log 2>&1
+echo [FILE] PonziPwn.sol | tee -a myth.log 
+myth analyze PonziPwn.sol >> myth.log 2>&1
+echo [FILE] PwnFoMo3D.sol | tee -a myth.log 
+myth analyze PwnFoMo3D.sol >> myth.log 2>&1
+echo [FILE] RC_KYC.sol | tee -a myth.log 
+myth analyze RC_KYC.sol >> myth.log 2>&1
+echo [FILE] RC_KYC_ADV.sol | tee -a myth.log 
+myth analyze RC_KYC_ADV.sol >> myth.log 2>&1
+echo [FILE] RatScam.sol | tee -a myth.log 
+myth analyze RatScam.sol >> myth.log 2>&1
+echo [FILE] SVIndex.sol | tee -a myth.log 
+myth analyze SVIndex.sol >> myth.log 2>&1
+echo [FILE] Sale1.sol | tee -a myth.log 
+myth analyze Sale1.sol >> myth.log 2>&1
+echo [FILE] SnowStorm.sol | tee -a myth.log 
+myth analyze SnowStorm.sol >> myth.log 2>&1
+echo [FILE] SpicyPot.sol | tee -a myth.log 
+myth analyze SpicyPot.sol >> myth.log 2>&1
+echo [FILE] SuperFoMo3D.sol | tee -a myth.log 
+myth analyze SuperFoMo3D.sol >> myth.log 2>&1
+echo [FILE] TokedoDaico.sol | tee -a myth.log 
+myth analyze TokedoDaico.sol >> myth.log 2>&1
+echo [FILE] USDTieredSTOFactory.sol | tee -a myth.log 
+myth analyze USDTieredSTOFactory.sol >> myth.log 2>&1
+echo [FILE] UpgradeabilityProxyFactory.sol | tee -a myth.log 
+myth analyze UpgradeabilityProxyFactory.sol >> myth.log 2>&1
+echo [FILE] WorldFomo.sol | tee -a myth.log 
+myth analyze WorldFomo.sol >> myth.log 2>&1
+echo [FILE] X3Dlong.sol | tee -a myth.log 
+myth analyze X3Dlong.sol >> myth.log 2>&1
+echo [FILE] ZaynixKey.sol | tee -a myth.log 
+myth analyze ZaynixKey.sol >> myth.log 2>&1
+solc use 0.4.25
+echo [FILE] PvPCrash.sol | tee -a myth.log 
+myth analyze PvPCrash.sol >> myth.log 2>&1
+echo [FILE] Intel.sol | tee -a myth.log 
+myth analyze Intel.sol >> myth.log 2>&1
+echo [FILE] FomoSport.sol | tee -a myth.log 
+myth analyze FomoSport.sol >> myth.log 2>&1
+echo [FILE] FFEIF.sol | tee -a myth.log 
+myth analyze FFEIF.sol >> myth.log 2>&1
+echo [FILE] FastGameMultiplier.sol | tee -a myth.log 
+myth analyze FastGameMultiplier.sol >> myth.log 2>&1
+echo [FILE] Cryptolotto10Minutes.sol | tee -a myth.log 
+myth analyze Cryptolotto10Minutes.sol >> myth.log 2>&1
+echo [FILE] Cryptolotto1Day.sol | tee -a myth.log 
+myth analyze Cryptolotto1Day.sol >> myth.log 2>&1
+echo [FILE] Cryptolotto1Hour.sol | tee -a myth.log 
+myth analyze Cryptolotto1Hour.sol >> myth.log 2>&1
+echo [FILE] Cryptolotto6Hours.sol | tee -a myth.log 
+myth analyze Cryptolotto6Hours.sol >> myth.log 2>&1
+echo [FILE] Cryptolotto7Days.sol | tee -a myth.log 
+myth analyze Cryptolotto7Days.sol >> myth.log 2>&1
+echo [FILE] ESportsMainCrowdsale.sol | tee -a myth.log 
+myth analyze ESportsMainCrowdsale.sol >> myth.log 2>&1
+echo [FILE] buyable.sol | tee -a myth.log 
+myth analyze buyable.sol >> myth.log 2>&1
+echo [FILE] eSportsToken.sol | tee -a myth.log 
+myth analyze eSportsToken.sol >> myth.log 2>&1
+echo [FILE] MarriageRegistry.sol | tee -a myth.log 
+myth analyze MarriageRegistry.sol >> myth.log 2>&1
+echo [FILE] EtherTransferTo.sol | tee -a myth.log 
+myth analyze EtherTransferTo.sol >> myth.log 2>&1
+echo [FILE] fomo3D4eva.sol | tee -a myth.log 
+myth analyze fomo3D4eva.sol >> myth.log 2>&1
+echo [FILE] Blocklancer_Payment.sol | tee -a myth.log 
+myth analyze Blocklancer_Payment.sol >> myth.log 2>&1
+echo [FILE] AirdropTryer.sol | tee -a myth.log 
+myth analyze AirdropTryer.sol >> myth.log 2>&1
+echo [FILE] GasToken2.sol | tee -a myth.log 
+myth analyze GasToken2.sol >> myth.log 2>&1
+echo [FILE] TrinityContractCore.sol | tee -a myth.log 
+myth analyze TrinityContractCore.sol >> myth.log 2>&1
+echo [FILE] BalanceCheckerN.sol | tee -a myth.log 
+myth analyze BalanceCheckerN.sol >> myth.log 2>&1
+echo [FILE] WPPPresale.sol | tee -a myth.log 
+myth analyze WPPPresale.sol >> myth.log 2>&1
+echo [FILE] ComfixedToken.sol | tee -a myth.log 
+myth analyze ComfixedToken.sol >> myth.log 2>&1
