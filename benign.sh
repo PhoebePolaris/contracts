5bf901b7746b06be14bbcc5f1e077726f4a599eb
Ubuntu <ubuntu@ip-172-31-37-93.ec2.internal>
2019-12-06 11:24:47 +0000

    benign

---
+++ benign.sh
@@ -0,0 +1,170 @@
+solc use 0.4.13
+echo [FILE] WyvernAtomicizer.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs WyvernAtomicizer.sol >> sec2.log 2>&1 
+solc use 0.4.17
+echo [FILE] casinoProxy.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs casinoProxy.sol >> sec2.log 2>&1 
+solc use 0.4.21
+echo [FILE] UserRank.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs UserRank.sol >> sec2.log 2>&1 
+echo [FILE] MineableToken.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs MineableToken.sol >> sec2.log 2>&1 
+echo [FILE] Updater.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs Updater.sol >> sec2.log 2>&1 
+echo [FILE] CandyLand.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs CandyLand.sol >> sec2.log 2>&1 
+echo [FILE] CandyLandSale.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs CandyLandSale.sol >> sec2.log 2>&1 
+java -jar ~/git/securify/build/libs/securify.jar -fs AuctusTokenSale.sol >> sec2.log 2>&1 
+echo [FILE] RUNEToken.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs RUNEToken.sol >> sec2.log 2>&1 
+solc use 0.4.23
+echo [FILE] GetDecimals.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs GetDecimals.sol >> sec2.log 2>&1 
+echo [FILE] EternalStorageProxy.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs EternalStorageProxy.sol >> sec2.log 2>&1 
+echo [FILE] ParsecInitialShip.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs ParsecInitialShip.sol >> sec2.log 2>&1 
+solc use 0.4.24
+echo [FILE] AWinner.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs AWinner.sol >> sec2.log 2>&1 
+echo [FILE] AddressDeployer.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs AddressDeployer.sol >> sec2.log 2>&1 
+echo [FILE] AddressToken.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs AddressToken.sol >> sec2.log 2>&1 
+echo [FILE] AdminUpgradeabilityProxy.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs AdminUpgradeabilityProxy.sol >> sec2.log 2>&1 
+java -jar ~/git/securify/build/libs/securify.jar -fs BBFarm.sol >> sec2.log 2>&1 
+echo [FILE] CountTransferManagerFactory.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs CountTransferManagerFactory.sol >> sec2.log 2>&1 
+echo [FILE] CvcMigrator.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs CvcMigrator.sol >> sec2.log 2>&1 
+echo [FILE] CvcProxy.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs CvcProxy.sol >> sec2.log 2>&1 
+echo [FILE] F3DClick.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DClick.sol >> sec2.log 2>&1  
+echo [FILE] F3DGo.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DGo.sol >> sec2.log 2>&1 
+echo [FILE] F3DKeysCalcShort.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DKeysCalcShort.sol >> sec2.log 2>&1 
+echo [FILE] F3DLink.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DLink.sol >> sec2.log 2>&1 
+echo [FILE] F3DNEW.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DNEW.sol >> sec2.log 2>&1 
+echo [FILE] F3DPLUS.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DPLUS.sol >> sec2.log 2>&1 
+echo [FILE] F3DSHORT.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DSHORT.sol >> sec2.log 2>&1 
+echo [FILE] F3DSOON.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DSOON.sol >> sec2.log 2>&1 
+echo [FILE] F3DShop.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3DShop.sol >> sec2.log 2>&1 
+echo [FILE] F3Dultra.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs F3Dultra.sol >> sec2.log 2>&1  
+echo [FILE] FOMO3.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs FOMO3.sol >> sec2.log 2>&1 
+echo [FILE] FOMO3TEST.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs FOMO3TEST.sol >> sec2.log 2>&1 
+echo [FILE] FOMO3TEST1.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs FOMO3TEST1.sol >> sec2.log 2>&1 
+echo [FILE] FOMOO.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs FOMOO.sol >> sec2.log 2>&1 
+echo [FILE] FoMo100.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs FoMo100.sol >> sec2.log 2>&1 
+echo [FILE] Fomo3DQuick.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs Fomo3DQuick.sol >> sec2.log 2>&1 
+echo [FILE] Fomo60Min.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs Fomo60Min.sol >> sec2.log 2>&1 
+echo [FILE] GameOfSwords.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs GameOfSwords.sol >> sec2.log 2>&1 
+echo [FILE] IcoTokenUpgradeability.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs IcoTokenUpgradeability.sol >> sec2.log 2>&1 
+echo [FILE] ModuleRegistryProxy.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs ModuleRegistryProxy.sol >> sec2.log 2>&1 
+echo [FILE] MultiBuyer.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs MultiBuyer.sol >> sec2.log 2>&1 
+echo [FILE] MultiSeller.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs MultiSeller.sol >> sec2.log 2>&1  
+echo [FILE] OK3D.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs OK3D.sol >> sec2.log 2>&1 
+java -jar ~/git/securify/build/libs/securify.jar -fs OwnedUpgradeabilityProxy.sol >> sec2.log 2>&1 
+echo [FILE] POHMO.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs POHMO.sol >> sec2.log 2>&1 
+echo [FILE] POOHMO.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs POOHMO.sol >> sec2.log 2>&1 
+echo [FILE] POOHMOX.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs POOHMOX.sol >> sec2.log 2>&1 
+echo [FILE] PackagedApp.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PackagedApp.sol >> sec2.log 2>&1 
+echo [FILE] PercentageTransferManagerFactory.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PercentageTransferManagerFactory.sol >> sec2.log 2>&1 
+echo [FILE] PlayCoinKey.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PlayCoinKey.sol >> sec2.log 2>&1 
+echo [FILE] PoHMO4D.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PoHMO4D.sol >> sec2.log 2>&1 
+echo [FILE] PoaCrowdsale.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PoaCrowdsale.sol >> sec2.log 2>&1 
+echo [FILE] PoaManager.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PoaManager.sol >> sec2.log 2>&1 
+echo [FILE] PoaToken.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PoaToken.sol >> sec2.log 2>&1 
+echo [FILE] PonziPwn.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PonziPwn.sol >> sec2.log 2>&1 
+echo [FILE] PwnFoMo3D.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs PwnFoMo3D.sol >> sec2.log 2>&1 
+echo [FILE] RatScam.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs RatScam.sol >> sec2.log 2>&1 
+echo [FILE] Sale1.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs Sale1.sol >> sec2.log 2>&1 
+echo [FILE] SecurityTokenRegistryProxy.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs SecurityTokenRegistryProxy.sol >> sec2.log 2>&1 
+echo [FILE] SnowStorm.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs SnowStorm.sol >> sec2.log 2>&1 
+echo [FILE] SpicyPot.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs SpicyPot.sol >> sec2.log 2>&1 
+java -jar ~/git/securify/build/libs/securify.jar -fs SuperFoMo3D.sol >> sec2.log 2>&1 
+echo [FILE] USDTieredSTOFactory.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs USDTieredSTOFactory.sol >> sec2.log 2>&1 
+echo [FILE] UpgradeabilityProxyFactory.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs UpgradeabilityProxyFactory.sol >> sec2.log 2>&1 
+echo [FILE] WorldFomo.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs WorldFomo.sol >> sec2.log 2>&1 
+echo [FILE] X3Dlong.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs X3Dlong.sol >> sec2.log 2>&1 
+echo [FILE] ZaynixKey.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs ZaynixKey.sol >> sec2.log 2>&1 
+echo [FILE] fathercontract.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs fathercontract.sol >> sec2.log 2>&1 
+echo [FILE] EternalStorageProxyForAirdropper.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs EternalStorageProxyForAirdropper.sol >> sec2.log 2>&1 
+solc use 0.4.25
+echo [FILE] Intel.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs Intel.sol >> sec2.log 2>&1 
+echo [FILE] FomoSport.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs FomoSport.sol >> sec2.log 2>&1 
+echo [FILE] FFEIF.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs FFEIF.sol >> sec2.log 2>&1 
+echo [FILE] eSportsToken.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs eSportsToken.sol >> sec2.log 2>&1 
+echo [FILE] MarriageRegistry.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs MarriageRegistry.sol >> sec2.log 2>&1 
+echo [FILE] EtherTransferTo.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs EtherTransferTo.sol >> sec2.log 2>&1 
+echo [FILE] fomo3D4eva.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs fomo3D4eva.sol >> sec2.log 2>&1 
+echo [FILE] Blocklancer_Payment.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs Blocklancer_Payment.sol >> sec2.log 2>&1 
+echo [FILE] AirdropTryer.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs AirdropTryer.sol >> sec2.log 2>&1 
+echo [FILE] GasToken2.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs GasToken2.sol >> sec2.log 2>&1 
+echo [FILE] TrinityContractCore.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs TrinityContractCore.sol >> sec2.log 2>&1 
+java -jar ~/git/securify/build/libs/securify.jar -fs Protocol.sol >> sec2.log 2>&1 
+echo [FILE] BalanceCheckerN.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs BalanceCheckerN.sol >> sec2.log 2>&1 
+java -jar ~/git/securify/build/libs/securify.jar -fs WPPPresale.sol >> sec2.log 2>&1 
+echo [FILE] Airdroper.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs Airdroper.sol >> sec2.log 2>&1 
+echo [FILE] ComfixedToken.sol | tee -a sec2.log 
+java -jar ~/git/securify/build/libs/securify.jar -fs ComfixedToken.sol >> sec2.log 2>&1 
